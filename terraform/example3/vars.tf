1fe885b176e8f81ca918ca9bd2359984a3c3ea10
aenisha <51830281+aenisha@users.noreply.github.com>
2024-09-22 09:22:51 +0545

    Create vars.tf

---
+++ terraform/example3/vars.tf
@@ -0,0 +1,19 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+variable "AMI" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-0e86e20dae9224db8"
+    us-east-2 = "ami-085f9c64a9b75eed5"
+  }
+}
+
+variable "USER" {
+  default = "ubuntu"
+}
