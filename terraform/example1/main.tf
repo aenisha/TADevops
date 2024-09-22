b178f2bec6a16f427976ec644ff5ee23bfbb2995
aenisha <51830281+aenisha@users.noreply.github.com>
2024-09-22 09:12:10 +0545

    main.tf

---
+++ terraform/example1/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2_instance_1" {
+  ami               = "ami-0e86e20dae9224db8"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = "devops-anisha"
+
+  vpc_security_group_ids = ["sg-0d967cfbe1ebefbee"]
+
+  tags = {
+    Name = "AnishaMaharjan"
+    CreatedBy = "Terraform"
+  }
+}
+
+output "Public_IP" {
+value = aws_instance.ec2_instance_1.public_ip
+}
+
+output "Private_IP" {
+value = aws_instance.ec2_instance_1.private_ip
+}
